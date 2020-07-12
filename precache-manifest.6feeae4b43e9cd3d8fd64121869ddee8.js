d0ee8f4f56851c5e518094c87fd1f5c3823d4f15
Andrew Keithly <andrew.keithly@gmail.com>
2020-07-12 00:51:44 -0500

    Updates

---
+++ precache-manifest.6feeae4b43e9cd3d8fd64121869ddee8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c14660b77b99c0071c9c47677a5ee011",
+    "url": "/index.html"
+  },
+  {
+    "revision": "565ef195666df6f445a0",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "f8d92ef15baae026534d",
+    "url": "/static/css/main.fec45a73.chunk.css"
+  },
+  {
+    "revision": "565ef195666df6f445a0",
+    "url": "/static/js/2.3c16ea9e.chunk.js"
+  },
+  {
+    "revision": "286624c8d95769e3759a2a7869634d07",
+    "url": "/static/js/2.3c16ea9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8d92ef15baae026534d",
+    "url": "/static/js/main.36820344.chunk.js"
+  },
+  {
+    "revision": "02abd13eaf1d7c33000d",
+    "url": "/static/js/runtime-main.6a771f16.js"
+  }
+]);
