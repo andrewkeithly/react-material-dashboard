4ee2cc12577d809c4cbe2f49af466813e738c06d
Andrew Keithly <andrew.keithly@gmail.com>
2020-07-15 11:51:52 -0500

    Updates

---
+++ precache-manifest.0c67e8505922c4c77be8202a59468d7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c7b519e43f1d43d687b3c995c85e6a",
+    "url": "/react-material-dashboard/index.html"
+  },
+  {
+    "revision": "55fa55e4e74619768152",
+    "url": "/react-material-dashboard/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "bed8db1a78e8dfa67160",
+    "url": "/react-material-dashboard/static/css/main.cbdf4783.chunk.css"
+  },
+  {
+    "revision": "55fa55e4e74619768152",
+    "url": "/react-material-dashboard/static/js/2.5b9f5aa0.chunk.js"
+  },
+  {
+    "revision": "286624c8d95769e3759a2a7869634d07",
+    "url": "/react-material-dashboard/static/js/2.5b9f5aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bed8db1a78e8dfa67160",
+    "url": "/react-material-dashboard/static/js/main.c5a0e1be.chunk.js"
+  },
+  {
+    "revision": "6b737b9a249efb2462b8",
+    "url": "/react-material-dashboard/static/js/runtime-main.d464c3b8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.97d72caaf25916ca12e4aa67261aa442.js"
+  "/react-material-dashboard/precache-manifest.0c67e8505922c4c77be8202a59468d7e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-material-dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
